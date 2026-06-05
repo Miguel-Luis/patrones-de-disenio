6c92fd6ae09047a86767d4e99dff90dbf6c89415
NohitaFresita <majalo17_98@hotmail.com>
2026-06-05 17:59:43 -0500

    feat: implemento patron Chain of Rresponsibility

---
+++ comportamiento/chain-of-responsibility/src/handlers/Handler.java
@@ -0,0 +1,14 @@
+package handlers;
+
+import model.Request;
+
+public abstract class Handler {
+
+    protected Handler nextHandler;
+
+    public void setNextHandler(Handler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    public abstract void handleRequest(Request request);
+}
